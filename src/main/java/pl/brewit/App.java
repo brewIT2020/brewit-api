8e472cd20698833e144d4e6ea183039d47fb3bd3
Kamil Szerlag <kszerlag@si-ecommerce.pl>
2020-03-08 22:52:23 +0100

    Init commit

---
+++ src/main/java/pl/brewit/App.java
@@ -0,0 +1,13 @@
+package pl.brewit;
+
+import io.javalin.Javalin;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        Javalin app = Javalin.create().start(7000);
+        app.get("/", ctx -> ctx.result("Hello World"));
+    }
+}
